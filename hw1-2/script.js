7c1c1830c1a2c9d22edc77320cedfce2eac27ca0
kangm1nse0 <alstj2004102@naver.com>
2025-05-05 05:53:22 +0900

    강민서 과제

--- hw1-2/script.js
+++ hw1-2/script.js
@@ -1,1 +1,15 @@
-//여기에 코드를 작성해주세요!
+const openb = document.querySelector(".open");
+const closeb = document.querySelector(".close");
+const menub = document.querySelector(".menu");
+
+closeb.addEventListener("click", function(){
+    menub.classList.add("active");
+    openb.style.display = "inline";
+    closeb.style.display = "none";
+});
+
+openb.addEventListener("click", function(){
+    menub.classList.remove("active");
+    openb.style.display = "none";
+    closeb.style.display = "inline";
+});
